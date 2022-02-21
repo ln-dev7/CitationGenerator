851b3cd2be7e48e12df45ae6265b44b4d03aa666
IDRISSHACKER <idrisscoder@gmail.com>
2022-02-21 19:46:54 +0100

    Refractoring du code

--- scripts/app.js
+++ scripts/app.js
@@ -1,53 +1,63 @@
 import { citations } from "./citations.js";
 
-const DERNIERR_CITATION_STR = "dernierreCitation"
-const DERNIERR_JOUR_STR     = "dernierJour"
-const INITIAL_CITATION      = 0
+const LAST_CITATION_STRING = "dernierreCitation"
+const LAST_DAY_STRING = "dernierJour"
+const INITIAL_CITATION = 0
+const CITATION_STEP = 1
+const CITATION_INDEX = 0
+const AUTOR_INDEX = 1
+const DATE_INDEX = 0
 
-window.onload = ()=>{
-let citation = document.querySelector(".citation");
-let auteur = document.querySelector(".auteur");
-let date = document.querySelector(".date");
+window.onload = () => {
+  const citation = document.querySelector(".citation")
+  const auteur = document.querySelector(".auteur")
+  const date = document.querySelector(".date")
 
-const day = new Date()
-const currentDay = day.toLocaleString().split(",")[INITIAL_CITATION]
+  const day = new Date()
 
-let dernierreCitation = INITIAL_CITATION
-let dernierJour = ""
+  const currentDay = day.toLocaleString().split(",")[DATE_INDEX]
+  const myCitationOfDay = getCitationOfDay(currentDay)
 
-if(localStorage.getItem(DERNIERR_CITATION_STR) && localStorage.getItem(DERNIERR_JOUR_STR)){
-  dernierreCitation = parseInt(localStorage.getItem(DERNIERR_CITATION_STR))
-  dernierJour = localStorage.getItem(DERNIERR_JOUR_STR)
-  if (dernierJour !== currentDay) {
-    dernierreCitation = dernierreCitation + 1
-    if (dernierreCitation <= citations.length) {
-      localStorage.setItem(DERNIERR_CITATION_STR, dernierreCitation)
-    }else {
-      dernierreCitation = INITIAL_CITATION
-      localStorage.setItem(DERNIERR_CITATION_STR, INITIAL_CITATION)
+  const dateLocale = day.toLocaleString("fr-FR", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  })
+
+  citation.textContent = citations[myCitationOfDay][CITATION_INDEX]
+  auteur.textContent = citations[myCitationOfDay][AUTOR_INDEX]
+  date.innerHTML = dateLocale;
+
+}
+
+/**
+ * @param currentDay int
+ * @return dernierreCitation int
+ */
+const getCitationOfDay = (currentDay) => {
+  let dernierreCitation = INITIAL_CITATION
+  let dernierJour = ""
+
+  if (localStorage.getItem(LAST_CITATION_STRING) && localStorage.getItem(LAST_DAY_STRING)) {
+    dernierreCitation = parseInt(localStorage.getItem(LAST_CITATION_STRING))
+    dernierJour = localStorage.getItem(LAST_DAY_STRING)
+    if (dernierJour !== currentDay) {
+      dernierreCitation = dernierreCitation + CITATION_STEP
+      if (dernierreCitation <= citations.length) {
+        localStorage.setItem(LAST_CITATION_STRING, dernierreCitation)
+      } else {
+        dernierreCitation = INITIAL_CITATION
+        localStorage.setItem(LAST_CITATION_STRING, INITIAL_CITATION)
+      }
+      localStorage.setItem(LAST_DAY_STRING, currentDay)
     }
-    localStorage.setItem(DERNIERR_JOUR_STR, currentDay)
+  } else {
+    localStorage.setItem(LAST_CITATION_STRING, INITIAL_CITATION)
+    localStorage.setItem(LAST_DAY_STRING, currentDay)
   }
-} else {
-  localStorage.setItem(DERNIERR_CITATION_STR, INITIAL_CITATION)
-  localStorage.setItem(DERNIERR_JOUR_STR, currentDay)
+
+  return dernierreCitation
+
 }
-
-let dateValue = new Date();
-
-citation.textContent = citations[dernierreCitation][0];
-auteur.textContent = citations[dernierreCitation][1];
-
-dateValue = new Date();
-
-let dateLocale = dateValue.toLocaleString("fr-FR", {
-  weekday: "long",
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-});
-
-date.innerHTML = dateLocale;
-}
-
 
