c7b7fe4c19508cafa0937badd7c8867549e20377
IDRISSHACKER <idrisscoder@gmail.com>
2022-02-22 09:11:24 +0100

    Implementation du web workers pour le travail hors connexion et pour transformer le site application de bureau et ajout des notification push

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,23 @@
 import { citations } from "./citations.js";
 
+const APP_NAME = "Citation Generator"
 const LAST_CITATION_STRING = "dernierreCitation"
 const LAST_DAY_STRING = "dernierJour"
+const HOUR_STRING = "hour"
 const INITIAL_CITATION = 0
 const CITATION_STEP = 1
 const CITATION_INDEX = 0
 const AUTOR_INDEX = 1
 const DATE_INDEX = 0
+const BASE_URL = location.protocol + "//" + location.host + "/"
+const ICON_PATH = `${BASE_URL}img/icons/512.png`
+const DATE_LANG = "fr-FR"
+const DATE_FORMAT = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+}
 
 window.onload = () => {
   const citation = document.querySelector(".citation")
@@ -14,20 +25,20 @@ window.onload = () => {
   const date = document.querySelector(".date")
 
   const day = new Date()
-
   const currentDay = day.toLocaleString().split(",")[DATE_INDEX]
+  const currentHour = day.toLocaleString().split(",")[1].split(":")[0]
   const myCitationOfDay = getCitationOfDay(currentDay)
+  const dateLocale = day.toLocaleString(DATE_LANG, { ...DATE_FORMAT })
 
-  const dateLocale = day.toLocaleString("fr-FR", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  })
+  const title = citations[myCitationOfDay][CITATION_INDEX]
+  const author = citations[myCitationOfDay][AUTOR_INDEX]
 
-  citation.textContent = citations[myCitationOfDay][CITATION_INDEX]
-  auteur.textContent = citations[myCitationOfDay][AUTOR_INDEX]
-  date.innerHTML = dateLocale;
+  showNotificationByHour(title, author, currentHour)
+  checkConnexion()
+
+  citation.textContent = title
+  auteur.textContent = author
+  date.innerHTML = dateLocale
 
 }
 
@@ -61,3 +72,70 @@ const getCitationOfDay = (currentDay) => {
 
 }
 
+/**
+ * @param title String
+ * @param msg String
+ * @return show notifcation
+ */
+const showNotification = (title, msg) => {
+  const granted = "granted"
+  const denied = "denied"
+  if (Notification.permission == granted) {
+    const notification = new Notification(title, {
+      body: msg,
+      icon: ICON_PATH
+    })
+    notification.onclick = () => {
+      window.location = BASE_URL
+    }
+  } else if (Notification.permission !== denied) {
+    Notification.requestPermission().then(permission => {
+      console.log(permission)
+    })
+  }
+}
+
+/**
+ * @param author String
+ * @param content String
+ * @param currentHour String
+ * @return showNotif
+ */
+const showNotificationByHour = (author, content, currentHour) => {
+
+  currentHour = parseInt(currentHour)
+  if (localStorage.getItem(HOUR_STRING)) {
+    const storageHour = parseInt(localStorage.getItem(HOUR_STRING))
+    if (storageHour !== currentHour) {
+      localStorage.setItem(HOUR_STRING, currentHour)
+      showNotification(author, content)
+    }
+  } else {
+    localStorage.setItem(HOUR_STRING, currentHour)
+    showNotification(author, content)
+  }
+
+}
+
+const checkConnexion = () => {
+  const verifyIfOffLine = () => {
+    const ifNotOnLine = window.setInterval(() => {
+      if (!navigator.onLine) {
+        showNotification(APP_NAME, "Vous êtes hors connexions, navigation hors connexion activées")
+        clearTimeout(ifNotOnLine)
+        verifyIfOnLine()
+      }
+    }, 500)
+  }
+  const verifyIfOnLine = () => {
+    const ifOnLine = window.setInterval(() => {
+      if (navigator.onLine) {
+        window.location = BASE_URL
+        showNotification(APP_NAME, "Connexion retablie")
+        clearTimeout(ifOnLine)
+        verifyIfOffLine()
+      }
+    }, 500)
+  }
+  verifyIfOffLine()
+}
---
+++ sw.js
@@ -0,0 +1,85 @@
+const PREFIX = "V19"
+const BASE = location.protocol + "//" + location.host
+const cached_FILES = [
+    `${BASE}/style.css`,
+    `${BASE}/offline.html`,
+    `${BASE}/favicon.ico`,
+    `${BASE}/img/github-fill.svg`,
+    `${BASE}/scripts/app.js`,
+    `${BASE}/scripts/citations.js`
+
+]
+
+const LAZY_CACHE = [`${BASE}/img/github-fill.svg`, `${BASE}/scripts/app.js`, `${BASE}/scripts/citations.js`]
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            (async () => {
+                try {
+                    const preloadResponse = await event.preloadResponse
+                    if (preloadResponse) {
+                        return preloadResponse
+                    }
+                    return await fetch(event.request)
+                } catch (e) {
+                    const cache = await caches.open(PREFIX)
+                    return await cache.match("/offline.html")
+                }
+            })()
+        )
+    } else if (cached_FILES.includes(event.request.url)) {
+        event.respondWith(caches.match(event.request))
+    } else if (LAZY_CACHE.includes(event.request.url)) {
+        event.respondWith(
+            (async () => {
+                try {
+                    const cache = await caches.open(PREFIX)
+                    const preloadResponse = await event.preloadResponse
+                    if (preloadResponse) {
+                        cache.put(event.request, preloadResponse.clone())
+                        return preloadResponse
+                    }
+                    const networkResponse = await fetch(event.request)
+                    cache.put(event.request, networkResponse.clone())
+                    return networkResponse
+                } catch (e) {
+                    return await caches.match(event.request)
+                }
+            })()
+        )
+    }
+})
+
+self.addEventListener("install", (event) => {
+    self.skipWaiting()
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open(PREFIX)
+            await Promise.all(cached_FILES.map((path) => {
+                return cache.add(new Request(path))
+            }))
+        })())
+})
+
+self.addEventListener("activate", (event) => {
+    clients.claim()
+    event.waitUntil((async () => {
+        const keys = await caches.keys()
+        await Promise.all(
+            keys.map((key) => {
+                if (!key.includes(PREFIX)) {
+                    return caches.delete(key)
+                }
+            })
+        )
+    }))
+})
+
+self.addEventListener("push", (event) => {
+    event.waitUntil(
+        self.registration.showNotification("Salut", {
+            body: "Voici un message"
+        })
+    )
+})
